365ef31191be4dabc0e18e3718064c56dc3c83c4
jhoan jimenez <jhoanjimz2@gmail.com>
2023-01-27 13:37:32 -0500

    SplashScreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@capacitor/splash-screen';
+
 import { AboutService } from './services/about.service';
 import { CalificarService } from './services/calificar.service';
 import { LenguajeService } from './services/lenguaje.service';
@@ -11,16 +14,23 @@ import { SolicitarTaxiService } from './services/solicitar-taxi.service';
 })
 export class AppComponent {
   constructor(
+    private platform: Platform,
     private solicitarTaxiService: SolicitarTaxiService,
     private aboutService: AboutService,
     private calificarService: CalificarService,
     private lenguajeService: LenguajeService
   ) {
-    this.lenguajeService.inicializarArchivos();
-    this.lenguajeService.setearIdiomaInicial();
-    this.solicitarTaxiService.getAplicaciones();
-    this.solicitarTaxiService.getTelefonos();
-    this.aboutService.getAbout();
-    this.calificarService.getOpcionesCalificar();
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(async () => {
+      await SplashScreen.show({ showDuration: 3000, autoHide: true });
+      this.lenguajeService.inicializarArchivos();
+      this.lenguajeService.setearIdiomaInicial();
+      this.solicitarTaxiService.getAplicaciones();
+      this.solicitarTaxiService.getTelefonos();
+      this.aboutService.getAbout();
+      this.calificarService.getOpcionesCalificar();
+    });
   }
 }
